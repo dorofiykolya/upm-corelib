2d7a4ed1473ebb2847fce2b2a03702c05f87ee98
dorofiykolya <dorofiy.kolya@gmail.com>
2022-10-26 21:31:04 +0300

    1.11.0

--- Runtime/Services/UI/Windows/IUIWindowService.cs
+++ Runtime/Services/UI/Windows/IUIWindowService.cs
@@ -1,10 +1,11 @@
 using System;
 using Common;
 using Framework.Runtime.Core.Widgets;
+using Injections;
 
 namespace Framework.Runtime.Services.UI.Windows
 {
-    public interface IUIWindowService
+    public interface IUIWindowService : IResolve
     {
         UIWindowReference[] Opened { get; }
         UIWindowReference Open(Type type, Action<Widget> onOpen, object model);
